542bfdc96d5ea532375ed9fb21d02bae621ab261
Eduard <mansurov@technokratos.com>
2021-05-09 20:32:04 +0300

    remove functions for develop

--- src/main/java/tech/itparklessons/movies/controller/ImportMoviesController.java
+++ src/main/java/tech/itparklessons/movies/controller/ImportMoviesController.java
@@ -3,33 +3,27 @@ package tech.itparklessons.movies.controller;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import tech.itparklessons.movies.service.ImportMovieService;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.time.Instant;
 
 @RestController
-@RequestMapping("/")
 @RequiredArgsConstructor
 public class ImportMoviesController {
     private final ImportMovieService movieService;
 
     @PostMapping
     public void importMovies(@RequestParam MultipartFile file) throws IOException {
-        Instant start = Instant.now();
-
         final CSVParser parse = CSVFormat.DEFAULT
                 .withFirstRecordAsHeader()
                 .parse(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8));
 
-
         movieService.importMovies(parse.getRecords());
-
-        Instant finish = Instant.now();
-        System.out.println(finish.minusMillis(start.toEpochMilli()).getEpochSecond());
     }
 }
